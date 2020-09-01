10a59dcfe8a01bf0e0c2e51fbda7869d2cf5fdbd
baba_s <shotababa1206@gmail.com>
2020-09-01 19:42:11 +0900

    first commit

---
+++ Editor/AutoAssetImporterOnPlayModeStateChanged.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+
+namespace Kogane
+{
+	[InitializeOnLoad]
+	public static class AutoAssetImporterOnPlayModeStateChanged
+	{
+		public static string[] PathList { get; set; } = new string[0];
+
+		static AutoAssetImporterOnPlayModeStateChanged()
+		{
+			void OnChanged( PlayModeStateChange change )
+			{
+				if ( PathList == null ) return;
+				if ( change != PlayModeStateChange.ExitingEditMode ) return;
+
+				foreach ( var path in PathList )
+				{
+					AssetDatabase.ImportAsset( path, ImportAssetOptions.ForceUpdate );
+				}
+			}
+
+			EditorApplication.playModeStateChanged += OnChanged;
+		}
+	}
+}
